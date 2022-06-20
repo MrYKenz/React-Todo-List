bbc8310827f2cd899c91d96d289c72e943cfaaee
LucasJWilliams <67558139+LucasJWilliams@users.noreply.github.com>
2022-06-20 14:51:57 +0100

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react' 
 
 export default function About() {
     return (
         <React.Fragment>
-           <h1>About</h1>
+           <h1>Aboout</h1>
            <br></br>
            <p>Simple TodoList App created in React.</p>
            <p>Made for educational purposes.</p>
